18d32405046e5e52081aa12d7408dbc34a6aa8b8
ErElectical <mohittanwar786.mt@gmail.com>
2021-12-26 10:34:23 +0000

    inventory management system

---
+++ inventorymanagement.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace inventorymanagementsystem
+{
+    public class Movie
+    {
+        public string item { get; set; }
+        public int weight { get; set; }
+        public int priceperkg { get; set; }
+    }
+    class program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("serialization --------------------------------------------------------------------");
+            List<Movie> item = new List<Movie>
+            {
+                new Movie { item = "rice", weight = 50, priceperkg = 78 },
+            new Movie { item = "wheat", weight = 60, priceperkg = 89 },
+            new Movie { item = "pulses", weight = 78, priceperkg = 120 }
+
+
+
+            };
+            string result = JsonConvert.SerializeObject(item);
+
+            Console.WriteLine(result);
+            Console.WriteLine("\t Deserialization of object ");
+            List<Movie> newitem=JsonConvert.DeserializeObject<List<Movie>>(result);
+            foreach (var d in newitem)
+            {
+                Console.WriteLine("item : " + d.item + "  value is   " + d.weight * d.priceperkg);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
