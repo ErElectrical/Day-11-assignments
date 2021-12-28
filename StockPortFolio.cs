2d97465d0177503c577c2aa23759fd65d628c47b
ErElectical <mohittanwar786.mt@gmail.com>
2021-12-28 07:49:14 +0000

    Datamodel class for our stock managemnet json file

---
+++ StockPortFolio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement
+{
+    public class Stock
+    {
+        public String StockName;
+        public long NumberOfShare;
+        public long SharePrice;
+        public long TotalStockPrice;
+        public override string ToString()
+        {
+            string st = $"StockName: {StockName}\nNumberOfShare: {NumberOfShare}\nSharePrice: {SharePrice}\nTotalStockPrice: {TotalStockPrice}";
+            return st;
+        }
+    }
+    public class StockPortFolio
+    {
+        
+        
+            public List<Stock> Stocks;
+            public long GrandTotalStockPrice;
+        
+    }
+}
